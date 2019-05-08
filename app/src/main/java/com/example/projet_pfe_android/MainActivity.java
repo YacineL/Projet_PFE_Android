d40a086b087e61f979817a937f7c72156b4b7eed
Nouna321 <zaitsadoune@gmail.com>
2019-05-09 00:13:15 +0200

    Commit_Interfaces

--- app/src/main/java/com/example/projet_pfe_android/MainActivity.java
+++ app/src/main/java/com/example/projet_pfe_android/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
